d257d03dd2113a2f955d797dc5a83ef0e8672356
stevenjmarsh <sjm.atl@gmail.com>
2018-10-17 15:19:08 -0400

    adding products and creating indexes

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ var idbApp = (function() {
     return;
   }
 
-  var dbPromise = idb.open('couches-n-things', 2, function (upgradeDb) {
+  var dbPromise = idb.open('couches-n-things', 5, function (upgradeDb) {
     switch (upgradeDb.oldVersion) {
       case 0:
       // a placeholder case so that the switch block will
@@ -31,7 +31,20 @@ var idbApp = (function() {
         console.log('Creating the products object store');
         upgradeDb.createObjectStore('products', { keyPath: 'id' });
 
-      // TODO 4.1 - create 'name' index
+      case 2:
+        console.log('Creating a name index');
+        var store = upgradeDb.transaction.objectStore('products');
+        store.createIndex('name', 'name', { unique: true });
+
+      case 3:
+        console.log('Creating a price index');
+        var store = upgradeDb.transaction.objectStore('products');
+        store.createIndex('price', 'price');
+
+      case 4:
+        console.log('Creating a description index');
+        var store = upgradeDb.transaction.objectStore('products');
+        store.createIndex('description', 'description');
 
       // TODO 4.2 - create 'price' and 'description' indexes
 
@@ -42,7 +55,76 @@ var idbApp = (function() {
 
   function addProducts() {
 
-    // TODO 3.3 - add objects to the products store
+    dbPromise.then(function (db) {
+      var tx = db.transaction('products', 'readwrite');
+      var store = tx.objectStore('products');
+      var items = [
+        {
+          name: 'Couch',
+          id: 'cch-blk-ma',
+          price: 499.99,
+          color: 'black',
+          material: 'mahogany',
+          description: 'A very comfy couch',
+          quantity: 3
+        },
+        {
+          name: 'Armchair',
+          id: 'ac-gr-pin',
+          price: 299.99,
+          color: 'grey',
+          material: 'pine',
+          description: 'A plush recliner armchair',
+          quantity: 7
+        },
+        {
+          name: 'Stool',
+          id: 'st-re-pin',
+          price: 59.99,
+          color: 'red',
+          material: 'pine',
+          description: 'A light, high-stool',
+          quantity: 3
+        },
+        {
+          name: 'Chair',
+          id: 'ch-blu-pin',
+          price: 49.99,
+          color: 'blue',
+          material: 'pine',
+          description: 'A plain chair for the kitchen table',
+          quantity: 1
+        },
+        {
+          name: 'Dresser',
+          id: 'dr-wht-ply',
+          price: 399.99,
+          color: 'white',
+          material: 'plywood',
+          description: 'A plain dresser with five drawers',
+          quantity: 4
+        },
+        {
+          name: 'Cabinet',
+          id: 'ca-brn-ma',
+          price: 799.99,
+          color: 'brown',
+          material: 'mahogany',
+          description: 'An intricately-designed, antique cabinet',
+          quantity: 11
+        }
+      ];
+      return Promise.all(items.map(function (item) {
+        console.log('Adding item: ', item);
+        return store.add(item);
+      })
+      ).catch(function (e) {
+        tx.abort();
+        console.log(e);
+      }).then(function () {
+        console.log('All items added successfully!');
+      });
+    });
 
   }
 
