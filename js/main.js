2a11830d4a242b3296b3a1dc2e1806e4a769010f
stevenjmarsh <sjm.atl@gmail.com>
2018-10-17 14:50:55 -0400

    creating db and object store, also removing eslint

--- js/main.js
+++ js/main.js
@@ -16,9 +16,29 @@ limitations under the License.
 var idbApp = (function() {
   'use strict';
 
-  // TODO 2 - check for support
+  if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+    return;
+  }
 
-  var dbPromise;
+  var dbPromise = idb.open('couches-n-things', 2, function (upgradeDb) {
+    switch (upgradeDb.oldVersion) {
+      case 0:
+      // a placeholder case so that the switch block will
+      // execute when the database is first created
+      // (oldVersion is 0)
+      case 1:
+        console.log('Creating the products object store');
+        upgradeDb.createObjectStore('products', { keyPath: 'id' });
+
+      // TODO 4.1 - create 'name' index
+
+      // TODO 4.2 - create 'price' and 'description' indexes
+
+      // TODO 5.1 - create an 'orders' object store
+
+    }
+  });
 
   function addProducts() {
 
